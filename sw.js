966e51b395f7eec8a17c779b5c298bff6f4252e5
wxads1997822-eng <wxads1997822@gmail.com>
2026-05-06 23:06:55 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", () => {
+  // Keep default network behavior; this file enables PWA detection.
+});
